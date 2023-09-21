07723a1663cd75721870abc5cfb759a7a7398b89
diegojsa <diegojsaraujo@gmail.com>
2023-09-21 12:59:44 -0300

    content criado

--- src/app/components/big-card/big-card.component.ts
+++ src/app/components/big-card/big-card.component.ts
@@ -13,4 +13,6 @@ export class BigCardComponent {
     cardTitle: string = '';
     @Input()
     cardDescription: string = '';
+    @Input()
+    id: string = "0";
 }
---
+++ src/app/data/dataFake.ts
@@ -0,0 +1,14 @@
+export const dataFake = [
+    {
+        "id": "1",
+        "photo": "https://criticalhits.com.br/wp-content/uploads/2019/10/tony-stark.webp",
+        "title": "Novo filme do Homem de Ferro em 3D",
+        "description": "Marvel anuncia novo filme de homem de ferro, confira"
+    },
+    {
+        "id": "2",
+        "photo": "https://disneyplusbrasil.com.br/wp-content/uploads/2021/07/Series-Marvel-Disney-Plus-1024x576.jpg",
+        "title": "Nova série anunciada no Disney Plus",
+        "description": "Olá mundo"
+    }
+]
